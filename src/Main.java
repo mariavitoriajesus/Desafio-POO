f464897069719ee25685cad45d445c338fd57d73
Maria <mariavitoria.jesus2003@gmail.com>
2023-08-08 20:41:49 -0300

    segund commi, Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import br.com.dio.Curso;
+import br.com.dio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("curso Angular");
+        curso1.setDescricao("descrição curso angular");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("Descrição da mentoria java");
+        mentoria.setData(LocalDate.now());
+
     }
 }
